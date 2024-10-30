a80931a50f2d46d9e5bec9a8d6d94075065cbf8d
Matthew Jackson <mjackson@unicon.net>
2024-10-30 13:12:14 -0500

    Change implementaiton of Get-NugetPackage to not use nuget.exe

--- logistics/scripts/modules/packaging/nuget-helper.psm1
+++ logistics/scripts/modules/packaging/nuget-helper.psm1
@@ -59,50 +59,35 @@ function Install-NuGetCli {
     return $nuget
 }
 
-function Get-NuGetPackage {
-    <#
-    .SYNOPSIS
-        Download and unzip a NuGet package for the purpose of bundling into another package.
-    .DESCRIPTION
-        Uses nuget command line to download a NuGet package and unzip it into an output
-        directory. Uses the Ed-Fi Azure Artifacts package feed by default. Default output directory
-        is ./downloads.
-    .PARAMETER packageName
-        Alias "applicationId". Name of the package to download.
-    .PARAMETER packageVersion
-        Package version number. Can include pre-release information. Optional. If not
-        specified, installs the most recent full release version.
-    .PARAMETER toolsPath
-        The path in which to find the NuGet command line client.
-    .PARAMETER outputDirectory
-        The path into which the package is unzipped. Defaults to "./downloads".
-    .PARAMETER packageSource
-        The NuGet package source. Defaults to "https://pkgs.dev.azure.com/ed-fi-alliance/Ed-Fi-Alliance-OSS/_packaging/EdFi/nuget/v3/index.json".
-    .EXAMPLE
-        $parameters = @{
-            packageName = "EdFi.Suite3.Ods.WebApi"
-            packageVersion = "5.2.0-b11661"
-            toolsPath = "./tools"
-        }
-        Get-NuGetPackage @parameters
-    #>
+<#
+.SYNOPSIS
+    Downloads and extracts the latest compatible version of a NuGet package.
+
+.OUTPUTS
+    Directory name containing the downloaded files.
+
+.EXAMPLE
+    Get-NugetPackage -PackageName "EdFi.Suite3.RestApi.Databases" -OutputDirectory ".packages"  -PackageVersion "5.3.0"
+#>
+function Get-NugetPackage {
     [CmdletBinding()]
-    param (
-        [string]
+    [OutputType([String])]
+    param(
+        # Exact package name
         [Parameter(Mandatory = $true)]
-        [Alias("applicationId")]
+        [string]
         $PackageName,
 
+        # Extracted package output directory
+        [Parameter(Mandatory = $false)]
+        $OutputDirectory = './downloads',
+
+        # Exact package version
+        [Parameter(Mandatory = $true)]
         [string]
         $PackageVersion,
 
-        [string]
-        [Parameter(Mandatory = $true)]
-        $ToolsPath,
-
-        [string]
-        $OutputDirectory = './downloads',
-
+        # URL for the NuGet package feed
         [string]
         $PackageSource = "https://pkgs.dev.azure.com/ed-fi-alliance/Ed-Fi-Alliance-OSS/_packaging/EdFi/nuget/v3/index.json",
 
@@ -110,34 +95,123 @@ function Get-NuGetPackage {
         $ExcludeVersion
     )
 
-    $nuget = Install-NuGetCli $ToolsPath
-    $parameters = @(
-        "install", $PackageName,
-        "-source", $PackageSource,
-        "-outputDirectory", $OutputDirectory
-    )
-    if ($ExcludeVersion) {
-        $parameters += "-ExcludeVersion"
-    }
-    if ($PackageVersion) {
-        $parameters += "-version"
-        $parameters += $PackageVersion
+    # The first URL just contains metadata for looking up more useful services
+    $nugetServices = Invoke-RestMethod $PackageSource
+
+    $packageService = $nugetServices.resources `
+    | Where-Object { $_."@type" -like "PackageBaseAddress*" } `
+    | Select-Object -Property "@id" -ExpandProperty "@id"
+
+    $lowerId = $PackageName.ToLower()
+    $file = "$($lowerId).$($PackageVersion)"
+    $zip = "$($file).zip"
+
+    New-Item -Path $OutputDirectory -Force -ItemType Directory | Out-Null
+
+    Push-Location $OutputDirectory
+
+    if ($null -ne (Get-ChildItem $file -ErrorAction SilentlyContinue)) {
+        # Already exists, don't re-download
+        Pop-Location
+        return "$($OutputDirectory)/$($file)"
     }
 
-    if(Get-IsWindows){
-        Write-Host -ForegroundColor Magenta "$ToolsPath/nuget $parameters"
-        & "$ToolsPath/nuget" $parameters | Out-Null
-    }else {
-        Write-Host -ForegroundColor Magenta "mono $ToolsPath/nuget.exe $parameters"
-        & mono "$ToolsPath/nuget.exe" $parameters | Out-Null
+    try {
+        Invoke-RestMethod "$($packageService)$($lowerId)/$($PackageVersion)/$($file).nupkg" -OutFile $zip
+
+        Expand-Archive $zip -Force
+
+        Remove-Item $zip
+    }
+    catch {
+        throw $_
+    }
+    finally {
+        Pop-Location
     }
 
-    if ($ExcludeVersion) {
-        return Resolve-Path "$outputDirectory/$PackageName*" | Select-Object -Last 1
-    }
-
-    return Resolve-Path "$outputDirectory/$PackageName.$PackageVersion*" | Select-Object -Last 1
+    return Resolve-Path "$($OutputDirectory)/$($file)" | Select-Object -Last 1
 }
+
+# function Get-NuGetPackage {
+#     <#
+#     .SYNOPSIS
+#         Download and unzip a NuGet package for the purpose of bundling into another package.
+#     .DESCRIPTION
+#         Uses nuget command line to download a NuGet package and unzip it into an output
+#         directory. Uses the Ed-Fi Azure Artifacts package feed by default. Default output directory
+#         is ./downloads.
+#     .PARAMETER packageName
+#         Alias "applicationId". Name of the package to download.
+#     .PARAMETER packageVersion
+#         Package version number. Can include pre-release information. Optional. If not
+#         specified, installs the most recent full release version.
+#     .PARAMETER toolsPath
+#         The path in which to find the NuGet command line client.
+#     .PARAMETER outputDirectory
+#         The path into which the package is unzipped. Defaults to "./downloads".
+#     .PARAMETER packageSource
+#         The NuGet package source. Defaults to "https://pkgs.dev.azure.com/ed-fi-alliance/Ed-Fi-Alliance-OSS/_packaging/EdFi/nuget/v3/index.json".
+#     .EXAMPLE
+#         $parameters = @{
+#             packageName = "EdFi.Suite3.Ods.WebApi"
+#             packageVersion = "5.2.0-b11661"
+#             toolsPath = "./tools"
+#         }
+#         Get-NuGetPackage @parameters
+#     #>
+#     [CmdletBinding()]
+#     param (
+#         [string]
+#         [Parameter(Mandatory = $true)]
+#         [Alias("applicationId")]
+#         $PackageName,
+
+#         [string]
+#         $PackageVersion,
+
+#         [string]
+#         [Parameter(Mandatory = $true)]
+#         $ToolsPath,
+
+#         [string]
+#         $OutputDirectory = './downloads',
+
+#         [string]
+#         $PackageSource = "https://pkgs.dev.azure.com/ed-fi-alliance/Ed-Fi-Alliance-OSS/_packaging/EdFi/nuget/v3/index.json",
+
+#         [switch]
+#         $ExcludeVersion
+#     )
+
+#     $nuget = Install-NuGetCli $ToolsPath
+#     $parameters = @(
+#         "install", $PackageName,
+#         "-source", $PackageSource,
+#         "-outputDirectory", $OutputDirectory
+#     )
+#     if ($ExcludeVersion) {
+#         $parameters += "-ExcludeVersion"
+#     }
+#     if ($PackageVersion) {
+#         $parameters += "-version"
+#         $parameters += $PackageVersion
+#     }
+
+#     if(Get-IsWindows){
+#         Write-Host -ForegroundColor Magenta "$ToolsPath/nuget $parameters"
+#         & "$ToolsPath/nuget" $parameters | Out-Null
+#     }else {
+#         Write-Host -ForegroundColor Magenta "mono $ToolsPath/nuget.exe $parameters"
+#         & mono "$ToolsPath/nuget.exe" $parameters | Out-Null
+#     }
+
+#     if ($ExcludeVersion) {
+#         return Resolve-Path "$outputDirectory/$PackageName*" | Select-Object -Last 1
+#     }
+
+#     return Resolve-Path "$outputDirectory/$PackageName.$PackageVersion*" | Select-Object -Last 1
+# }
 
 $exports = @(
     "Install-NuGetCli"
